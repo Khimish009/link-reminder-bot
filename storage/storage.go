af2d414e28073ba868efb43ddf70d2b70c6c9daf
Сальман Химишев <salman.khimishev01@gmail.com>
2025-03-30 22:40:52 +0300

    feat(storage): add storage

---
+++ storage/storage.go
@@ -0,0 +1,34 @@
+package storage
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"link-reminder-bot/lib/e"
+)
+
+type Storage interface {
+	Save(p *Page) error
+	PickRandom(userName string) (*Page, error)
+	Remove(p *Page) error
+	IsExist(p *Page) (bool, error)
+}
+
+type Page struct {
+	URL string
+	UserName string
+}
+
+func (p Page) Hash() (string, error) {
+	hash := sha1.New()
+
+	if _, err := io.WriteString(hash, p.URL); err != nil {
+		return "", e.Wrap("can't canculate hash", err)
+	}
+
+	if _, err := io.WriteString(hash, p.UserName); err != nil {
+		return "", e.Wrap("can't canculate hash", err)
+	}
+
+	return fmt.Sprintf("%x", hash.Sum(nil)), nil
+}
